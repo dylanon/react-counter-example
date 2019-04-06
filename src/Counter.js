c5dda95d42916b9f88f8b90059aad3ff0599afdc
Dylan On <dylanon@users.noreply.github.com>
2019-04-06 11:09:09 -0400

    Switch tabs to spaces

--- src/Counter.js
+++ src/Counter.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // function Counter() {
-	// return <button>I have been clicked 0 times.</button>;
+// return <button>I have been clicked 0 times.</button>;
 // }
 
 class Counter extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			timesClicked: 0
-		}
-		this.handleClick = this.handleClick.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      timesClicked: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-	handleClick() {
-		this.setState({
-			timesClicked: this.state.timesClicked + 1
-		})
-	}
+  handleClick() {
+    this.setState({
+      timesClicked: this.state.timesClicked + 1,
+    });
+  }
 
-	render() {
-		return (
-			<button
-				onClick={this.handleClick}
-				style={{
-					margin: '1rem',
-					background: this.props.bgColor,
-					border: 0,
-					color: 'white'
-				}}
-			>
-				I have been clicked {this.state.timesClicked} times.
-			</button>
-		)
-	}
+  render() {
+    return (
+      <button
+        onClick={this.handleClick}
+        style={{
+          margin: "1rem",
+          background: this.props.bgColor,
+          border: 0,
+          color: "white",
+        }}
+      >
+        I have been clicked {this.state.timesClicked} times.
+      </button>
+    );
+  }
 }
 
 export default Counter;
